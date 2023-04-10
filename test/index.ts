93e79ce0a36593e13044754a8c6803dbcc7fd4dd
Tao <magicdawn@qq.com>
2023-04-11 03:28:05 +0800

    test: use network videos

--- test/index.ts
+++ test/index.ts
@@ -9,8 +9,10 @@ import {
   getVideoRotationSync,
 } from '../ts-src'
 
+import { duration, durationDisplay, file, fileRotated } from './setup'
+
 // TODO: add a sample video file
-const file = '/Users/magicdawn/Movies/曼达洛人.The.Mandalorian.S03E06.1080p.H265-NEW字幕组.mp4'
+// const file = '/Users/magicdawn/Movies/曼达洛人.The.Mandalorian.S03E06.1080p.H265-NEW字幕组.mp4'
 
 describe('ff-helper', () => {
   it('.configuration', () => {
@@ -18,23 +20,25 @@ describe('ff-helper', () => {
   })
 
   it('.getVideoDurationSync', () => {
-    getVideoDurationSync(file).should.eql(2654000)
+    getVideoDurationSync(file).should.approximately(duration, 1000)
   })
   it('.getVideoDuration', async () => {
-    ;(await getVideoDuration(file)).should.eql(2654000)
+    ;(await getVideoDuration(file)).should.approximately(duration, 1000)
   })
 
   it('.getVideoDurationDisplaySync', async () => {
-    getVideoDurationDisplaySync(file).should.eql('00:44:14')
+    getVideoDurationDisplaySync(file).should.eql(durationDisplay)
   })
   it('.getVideoDurationDisplay', async () => {
-    ;(await getVideoDurationDisplay(file)).should.eql('00:44:14')
+    ;(await getVideoDurationDisplay(file)).should.eql(durationDisplay)
   })
 
   it('.getVideoRotationSync', () => {
     getVideoRotationSync(file).should.eql(0)
+    getVideoRotationSync(fileRotated).should.eql(270)
   })
   it('.getVideoRotation', async () => {
     ;(await getVideoRotation(file)).should.eql(0)
+    ;(await getVideoRotation(fileRotated)).should.eql(270)
   })
 })
---
+++ test/setup.ts
@@ -0,0 +1,23 @@
+const url = 'https://download.pexels.com/vimeo/371817283/pexels-pressmaster-3195394.mp4'
+
+export const file = __dirname + '/sample-videos/sample.mp4'
+export const durationDisplay = '00:00:13'
+export const duration = 13000 // ms
+export const fileRotated = __dirname + '/sample-videos/sample-rotated-90.mp4'
+
+import { execSync } from 'child_process'
+import dl from 'dl-vampire'
+import { existsSync } from 'fs'
+
+before(async () => {
+  // HEAD not allowed
+  if (!existsSync(file)) {
+    await dl({ url, file })
+  }
+
+  // 90 clockwise
+  // ffmpeg -y -i 'sample.mp4' -c copy -metadata:s:v:0 rotate=270 'sample-rotated-90.mp4'
+  if (!existsSync(fileRotated)) {
+    execSync(`ffmpeg -y -i '${file}' -c copy -metadata:s:v:0 rotate=270 '${fileRotated}'`)
+  }
+})

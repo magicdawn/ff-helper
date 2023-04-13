bfce9bc831ab73f8f6dd72c08ec63f90c939950c
Tao <magicdawn@qq.com>
2023-04-14 07:29:11 +0800

    feat: [screengen] add sharp encode wrapper

--- demo/index.ts
+++ demo/index.ts
@@ -1,39 +1,32 @@
+// not working, use
+// export RUST_LOG='ff_helper=debug'
+process.env.RUST_LOG = 'ff_helper=debug'
+
 import fs from 'fs'
 import path from 'path'
-import sharp from 'sharp'
-import { getScreenshotAtSync, getVideoInfoSync } from '../addon'
+import {
+  configuration,
+  getVideoInfoSync,
+  license,
+  screengen,
+  version,
+  versionInfo,
+} from '../ts-src'
 
 const file = path.join(__dirname, '../test/sample-videos/sample-rotated-90.mp4')
 
 void (async () => {
-  // console.log(configuration())
-  // console.log(version())
-  // console.log(versionInfo())
-  // console.log(license())
-
-  // try {
-  // } catch (e) {
-  //   console.error('error:')
-  //   console.error('typeof e = ', typeof e)
-  //   console.error('typeof e.stack = ', typeof e.stack)
-  //   console.log('e.message = %s', e.message)
-  //   console.error(e.stack || e)
-  // }
+  console.log(configuration())
+  console.log(version())
+  console.log(versionInfo())
+  console.log(license())
 
   const info = getVideoInfoSync(file)
-  // console.log(info)
-  const b = getScreenshotAtSync(file, 8 * 1000)
-  // console.log(b.byteLength)
-  const jpegBuf = await sharp(b, {
-    raw: {
-      channels: 4,
-      width: info.width,
-      height: info.height,
-    },
-  })
-    .jpeg({ mozjpeg: true, quality: 85 })
-    .withMetadata()
-    .rotate(360 - info.rotation)
-    .toBuffer()
-  fs.writeFileSync(path.join(__dirname, '../test/sample-videos/sample-rotated-90-1s.jpg'), jpegBuf)
+  console.log(info)
+
+  const bufferSnapshot = await screengen(file, 1000)
+  fs.writeFileSync(
+    path.join(__dirname, '../test/sample-videos/sample-rotated-90-1s.jpg'),
+    bufferSnapshot
+  )
 })()
--- ts-src/index.ts
+++ ts-src/index.ts
@@ -1,9 +1,11 @@
-export * from '../addon'
-import * as addon from '../addon'
+import sharp from 'sharp'
 
 import dayjs from 'dayjs'
 import duration from 'dayjs/plugin/duration'
 dayjs.extend(duration)
+
+import * as addon from '../addon'
+export * from '../addon'
 
 export function getVideoDurationDisplaySync(file: string) {
   return displayMs(addon.getVideoDurationSync(file))
@@ -14,4 +16,33 @@ export async function getVideoDurationDisplay(file: string) {
 
 function displayMs(ms: number) {
   return dayjs.duration(ms, 'milliseconds').format('HH:mm:ss')
+}
+
+/**
+ * take a screenshot for video at given timestamp
+ * node+rust version of https://gitlab.com/opennota/screengen
+ *
+ * @param file - the video file
+ * @param ts - the given timestamp, in millseconds
+ *
+ * @returns Buffer encoded with mozjpeg, just write to a jpg or jpeg file
+ *
+ * @remarks the rust exported `getScreenshotAtSync` & `getScreenshotAt` returns raw pixel Buffer
+ */
+
+export async function screengen(file: string, ts: number) {
+  const info = await addon.getVideoInfo(file)
+  const pixelBuf = await addon.getScreenshotAt(file, ts)
+  const jpegBuf = await sharp(pixelBuf, {
+    raw: {
+      channels: 4,
+      width: info.width,
+      height: info.height,
+    },
+  })
+    .jpeg({ mozjpeg: true, quality: 85 })
+    .withMetadata()
+    .rotate(360 - info.rotation)
+    .toBuffer()
+  return jpegBuf
 }

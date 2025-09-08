8f038b0e21854779d08fcd8af33b22499bc13dc3
magicdawn <magicdawn@qq.com>
2025-09-08 16:59:11 +0800

    chore: rm unused files

--- demo/index.ts
+++ demo/index.ts
@@ -1,17 +1,10 @@
 // not working, use
 // export RUST_LOG='ff_helper=debug'
-process.env.RUST_LOG = 'ff_helper=debug'
+import fs from 'node:fs'
+import path from 'node:path'
+import { configuration, getVideoInfoSync, license, screengen, version, versionInfo } from '../ts-src'
 
-import fs from 'fs'
-import path from 'path'
-import {
-  configuration,
-  getVideoInfoSync,
-  license,
-  screengen,
-  version,
-  versionInfo,
-} from '../ts-src'
+process.env.RUST_LOG = 'ff_helper=debug'
 
 const file = path.join(__dirname, '../test/sample-videos/sample-rotated-90.mp4')
 
@@ -25,8 +18,5 @@ void (async () => {
   console.log(info)
 
   const bufferSnapshot = await screengen(file, 1000)
-  fs.writeFileSync(
-    path.join(__dirname, '../test/sample-videos/sample-rotated-90-1s.jpg'),
-    bufferSnapshot
-  )
+  fs.writeFileSync(path.join(__dirname, '../test/sample-videos/sample-rotated-90-1s.jpg'), bufferSnapshot)
 })()
